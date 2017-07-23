137829a7740d8731a20da4456f6985989a6467c1
ryu609 <m.joynauth@gmail.com>
2017-07-23 21:32:02 +0400

    get element modifications

--- HistoricApp/Program.cs
+++ HistoricApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,11 +18,11 @@ namespace HistoricApp
 
             var entry2  = new Entry(2, 2, DateTime.Now, 6550);
             var user2 = new User(2, 2, "Tanish", "Joynauth");
-            var department2 = new Department(2, "II", 1);
+            var department2 = new Department(2, "II", 2);
 
             var entry3 = new Entry(3, 1, entry1.DateCreated,6550, DateTime.Now, 6551);
-            var user3 = new User(1, 1, "Ayush", "Joynauth");
-            var department3 = new Department(1, "IT", 1);
+            var user3 = new User(3, 1, "Ayush", "Joynauth");
+            var department3 = new Department(1, "IT", 3);
 
             var myExtract = new List<Extract>()
             {
@@ -49,11 +50,16 @@ namespace HistoricApp
             var grouped = myExtract.GroupBy(i => i.Entry.EntryId);
             foreach (var group in grouped)
             {
-                Console.WriteLine(group.Key);
-                var createdDate = group.Select(g => g.Entry.DateCreated.ToString());
-                foreach (var date in createdDate)
+                Console.WriteLine("Entry Number" + group.Key);
+                Console.WriteLine("Creation Date" + group.Select(g => g.Entry.DateCreated).FirstOrDefault());
+
+                foreach (var myEntry in group.Select(s => s.Entry))
                 {
-                    Console.WriteLine(date);
+                    var myUser = group.FirstOrDefault(u => u.User.UserId == myEntry.HeaderId).User;
+                    var myDepartment = group.FirstOrDefault(u => u.Department.DepartmentId == myEntry.HeaderId).Department;
+                    Console.WriteLine(myEntry.HeaderId);
+                    Console.WriteLine(myUser.UserId + " - " + myUser.FirstName + " - " + myUser.LastName);
+                    Console.WriteLine(myDepartment.DepartmentId + " - " + myDepartment.DepartmentName);
                 }
             }
             Console.ReadLine();
